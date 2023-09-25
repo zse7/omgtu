9d2285b706c5d978415064fcdb4cd8979a123d19
zse7 <144302062+zse7@users.noreply.github.com>
2023-09-25 20:24:53 +0600

    Add files via upload

---
+++ algoritm/19.09.23 последовательность н элементов.cs
@@ -0,0 +1,92 @@
+﻿//количество элементов значение которых больше предыдущего
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int n, pr_num, num, ch = 0;
+        n = Convert.ToInt32(Console.ReadLine());
+        pr_num = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n - 1; i++)
+        {
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > pr_num)
+            {
+                ch++;
+            }
+        }
+        Console.WriteLine(ch);
+    }
+}
+
+//определить количество элементов значение которых меньше всех вводимых до него
+using System;
+class Posledovatelnost_n
+{
+    static void Main()
+    {
+        int n, x, y, min_el, ch = 0;
+        n = Convert.ToInt32(Console.ReadLine());
+        x = Convert.ToInt32(Console.ReadLine());
+        min_el = x;
+        for (int i = 1; i < n; i++)
+        {
+            y = Convert.ToInt32(Console.ReadLine());
+            if (y < min_el)
+            {
+                ch++;
+                min_el = y;
+            }
+        }
+        Console.WriteLine(ch);
+    }
+}
+
+//Определить количество элементов значение которых больше левого и правого эдемента
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int n, pr_num, num, sl_num, ch = 0;
+        n = Convert.ToInt32(Console.ReadLine());
+        pr_num = Convert.ToInt32(Console.ReadLine());
+        num = Convert.ToInt32(Console.ReadLine());
+        sl_num = Convert.ToInt32(Console.ReadLine());
+        if ((num > pr_num) & (num > sl_num)) ch++;
+        for (int i = 1; i < n - 2; i++)
+        {
+            pr_num = num;
+            num = sl_num;
+            sl_num = Convert.ToInt32(Console.ReadLine());
+            if ((num > pr_num) & (num > sl_num)) ch++;
+        }
+        Console.WriteLine(ch);
+    }
+}
+
+//Определить наибольшую длинну возрастающей подпоследовательности
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int n, x, y, max_podposled = 1, tec_podposled = 1;
+        n = Convert.ToInt32(Console.ReadLine());
+        x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i < n; i++)
+        {
+            y = Convert.ToInt32(Console.ReadLine());
+            if (y > x)
+            {
+                tec_podposled++;
+                max_podposled = tec_podposled;
+            }
+            else
+            {
+                tec_podposled = 1;
+            }
+        }
+        Console.WriteLine(max_podposled);
+    }
+}
